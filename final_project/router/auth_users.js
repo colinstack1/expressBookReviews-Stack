25312372d8b0ca9f3b5ab740fbe123513f45c7c4
Colin Stack <colinstackaa@gmail.com>
2026-05-11 23:07:15 -0400

    task 7 completed

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -14,9 +14,30 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+
+  if (!authenticatedUser(username, password)) {
+    return res.status(401).json({ message: "Invalid username or password" });
+  }
+
+  const accessToken = jwt.sign(
+    { data: username },
+    "access",
+    { expiresIn: 60 * 60 }
+  );
+
+  req.session.authorization = {
+    accessToken,
+    username
+  };
+
+  return res.status(200).json({ message: "User successfully logged in" });
 });
 
 // Add a book review
