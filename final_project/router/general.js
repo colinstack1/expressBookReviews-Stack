51bba4b234b2dfaa1da45102be5b4da1821894b4
Colin Stack <colinstackaa@gmail.com>
2026-05-11 22:39:52 -0400

    completed task1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,7 +12,7 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
+  return res.status(200).send(JSON.stringify(books, null, 4));
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
@@ -24,7 +24,13 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
+    const isbn = req.params.isbn;
+
+    if (books[isbn]) {
+        return res.status(200).json(books[isbn]);
+    }
+    
+    return res.status(404).json({ message: "Book not found" });
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
